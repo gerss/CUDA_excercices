486437ce98f2b35b1eb4b7373e6d00e8092abb94
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-03-23 17:53:32 -0300

    Adheri __global_ void a la función

--- VecAdd.cu
+++ VecAdd.cu
@@ -1,5 +1,7 @@
 // Kernel definition
+
 __global__ void VecAdd(float* A, float* B, float* C)
+
 {
 	int i = threadIdx.x;
 	C[i] = A[i] + B[i];
@@ -7,8 +9,12 @@ __global__ void VecAdd(float* A, float* B, float* C)
 
 int main()
 {
-	...
+	const int N  = 10;
+	double A[], B[], C[];
+	
+	__global__ void VecAdd( A, B, C);
+	
 	// Kernel invoaction with N threads
 	VecAdd<<<1, N>>>(A,B,C);
-	...
+	
 }
